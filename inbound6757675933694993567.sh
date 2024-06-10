f9a7ef09e5b9f3b7fb220778fefa2579d316cccd
Mtandat78 <mtandat0708@gmail.com>
2024-06-10 13:09:39 +0700

    rpcrun.sh

--- inbound6757675933694993567.sh
+++ inbound6757675933694993567.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w YOUR_ADDRESS -r YOUR_NODE:YOUR_PORT -p rpc;
+    ./astrominer -w deroi1qy9al37a8qgjmat4y9wf5wc637md58jtt6p4980k34xxhrk2h9m6jq9pvfz92xcqqqqcl02dw0uqec6ywq -r community-pools.mysrv.cloud:10300 -p rpc;
     sleep 5;
 done
